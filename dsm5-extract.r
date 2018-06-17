d366cf8c1337deb9d9fb8a028ac2a90bc4660909
enilsson <eeenilsson@hotmail.com>
2018-06-18 01:14:14 +0200

    ok

--- dsm5-extract.r
+++ dsm5-extract.r
@@ -3,6 +3,40 @@
 ## packages
 pacman::p_load(tidyverse, tabulizer, rJava)
 
+## Functions
+spellCorrect <- function(SPELL, CHUNK){
+    ## Replaces matches in CHUNK from SPELL list. SPELL is a list of named elements with name = match and value = replacement 
+    TEMP <- CHUNK
+    for (i in 1:length(SPELL)){
+    TEMP <-
+        gsub(names(SPELL[i]), SPELL[i][[1]], TEMP, perl=T)
+    }
+    return(TEMP)    
+}
+
+makeHeaders <-function(x){
+    gsub("((?<=\n)(?:(?![\\,\\.]).)+(?=\n){1}?)", "## \\1", x, perl=T)
+}
+
+
+## variables
+
+### make a listr of value-replacement pairs
+spellList <- list(
+    'Reiationship'= "Relationship",
+    'Prevaience'= "Prevalence",
+    'Deveiopment' = "Development",
+    'Reiated' = "Related",
+    'iVlaricers' = "Markers",
+    'lUloderate' = "Moderate",
+    'Cuiture' = "Culture",
+    'comotbid' = "comorbid",
+    ' ̂' = "",
+    '(-){2,}' = "",
+    '(_){2,}' = "",
+    '(_̂){1,}' = ""
+)
+
 ### DSM-5 (not the updated version)
 contents <-
     extract_text("/home/eee/Dropbox/psykiatri/documents/dsm-5-manual-2013.pdf", pages = 5:6) ## contents
@@ -52,23 +86,23 @@ section2a <- paste(section2a, collapse = "") ## collapse to one
 ## remove - at end of line
 section2a <- gsub("­\n", "", section2a)
 
-section2a <- gsub("(?s)\n(?!\\(|[A-Z]|[0-9])", "", section2a, perl=T) ## remove \n not followed by parens or CAP or number
+section2a <- gsub("(?s)\n(?!\\(|[A-Z]|[0-9]\\.)", "", section2a, perl=T) ## remove \n not followed by parens or CAP or number
 
-## identify headers as lines without punctuation characters
-section2a <- gsub("((?<=\n)(?:(?![[:punct:]]).)+(?=\n){1}?)", "## \\1", section2a, perl=T)
+## identify headers as lines without comma or dot
+section2a <- gsub("((?<=\n)(?:(?![\\,\\.]).)+(?=\n){1}?)", "## \\1", section2a, perl=T)
 
 ## format details
 section2a <-
     gsub("Specify(.*:\n)", "Specify\\1", section2a)
 section2a <- gsub("\n", "\n\n", section2a) ## double newline
-### Spelling
-section2a <- gsub("Reiationship", "Relationship", section2a)
-section2a <- gsub("Deveiopment", "Development", section2a)
-section2a <- gsub("Reiated", "Related", section2a)
-section2a <- gsub("iVlaricers", "Markers", section2a)
-section2a <- gsub("lUloderate", "Moderate", section2a)
-## Prevaience
-## Cuiture-Reiated
+
+## replace matches in spellList
+## for (i in 1:length(spellList)){
+##     section2a <-
+##         gsub(names(spellList[i]), spellList[i][[1]], section2a)
+## }
+
+section2a <- spellCorrectTEST(spellList, section2a) ## fix spelling
 
 ## chunk specific replacements
 section2a <- gsub("(?s)\\.&.*Æu cru", "", section2a, perl = T) ## remove table
@@ -98,20 +132,36 @@ sectionTEST <- paste(sectionTEST, collapse = "") ## collapse to one
 ## remove - at end of line
 sectionTEST <- gsub("­\n", "", sectionTEST)
 
-sectionTEST <- gsub("(?s)\n(?!\\(|[A-Z]|[0-9])", "", sectionTEST, perl=T) ## remove \n not followed by parens or CAP or number
-
-## identify headers as lines without punctuation characters
-sectionTEST <- gsub("((?<=\n)(?:(?![[:punct:]]).)+(?=\n){1}?)", "## \\1", sectionTEST, perl=T)
-
-## format details
+## remove surplus newlines
 sectionTEST <-
-    gsub("Specify(.*:\n)", "Specify\\1", sectionTEST)
+    gsub("(?s)\n(?!\\(|[A-Z]|[0-9]\\.|[0-9]{3,}.*\\(F)", "", sectionTEST, perl=T) 
+## removes \n not followed by parens or CAP or number followed by dot
+
+## test
+## sectionTEST <-
+    ## gsub("(?s)\n(?!\\(|[A-Z]|[0-9]\\.|[0-9]{3,}.*\\(F)", "", "Unknown) \nSubstance-Induced Disorders\nBecause \nanew.", perl=T) 
+
+## identify headers as lines without dot or comma characters or a special expression for some lists of diagnosis codes
+## sectionTEST <-
+##     gsub("((?<=\n)(?:(?![\\,\\.]).)+(?=\n){1}?)", "## \\1", sectionTEST, perl=T)
+
+## headers
+sectionTEST <- makeHeaders(sectionTEST)
+
+sectionTEST <- spellCorrect(spellList, sectionTEST) ## fix spelling
+
 sectionTEST <- gsub("\n", "\n\n", sectionTEST) ## double newline
 
-sectionTEST <- gsub("(-){2,}", "", sectionTEST, perl=T)
-sectionTEST <- gsub("(_){2,}", "", sectionTEST, perl=T)
-sectionTEST <- gsub("(_̂){1,}", "", sectionTEST, perl=T)
+## format details
+## sectionTEST <-
+##     gsub("Specify(.*:\n)", "Specify\\1", sectionTEST)
+## sectionTEST <- gsub("(-){2,}", "", sectionTEST, perl=T)
+## sectionTEST <- gsub("(_){2,}", "", sectionTEST, perl=T)
+## sectionTEST <- gsub("(_̂){1,}", "", sectionTEST, perl=T)
 
 writeLines(sectionTEST[1], "sectionTEST.txt")
+
+
+
 
 x
