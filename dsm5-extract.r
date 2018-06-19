45bd79e3692ae2e279ea12b8af84926ce494abeb
enilsson <eeenilsson@hotmail.com>
2018-06-19 10:54:33 +0200

    edit classifications

--- dsm5-extract.r
+++ dsm5-extract.r
@@ -115,30 +115,19 @@ taskforce <- paste(taskforce, collapse = "") ## collapse to one
 
 ## ## Classifications (List of diagnoses and sub-diagnoses, import not working)
 ## Note: import not working, codes in sequence instead of per line
-## classifications <-     extract_text("/home/eee/Dropbox/psykiatri/documents/dsm-5-manual-2013.pdf", pages = 13:40) ## classifications list start on p13
-## classifications <- paste(classifications, collapse = "") ## collapse to one
-## classifications <- cleanNewlines(classifications) ## not working here so well
-## writeLines(classifications, "classifications.txt")
-## gsub("((?<=\n).*\\)(?=\n))", "## \\1", classifications, perl=T)
+classifications <-     extract_text("/home/eee/Dropbox/psykiatri/documents/dsm-5-manual-2013.pdf", pages = 13:40) ## classifications list start on p13
+classifications <- paste(classifications, collapse = "") ## collapse to one
+classifications <- cleanNewlines(classifications) ## not working here so well
+writeLines(classifications, "classifications.txt")
 
-## make list of codes (use existing csv instead?)
-## classificationsList <- classifications
-## classificationsList[1] <- gsub("(?s).*(?=\nNe)", "", classificationsList[1], perl=T)
-## ## strsplit(classificationsList[1])
-## ## strsplit(classificationsList[1], "\n") ## split by newline
-
-## ## multiple pages from list
-## for(i in 1:length(classificationsList)){
-##     write(classificationsList[[i]][[1]], file = "test.txt"
-##        , append = TRUE
-##           )
-    
-## }
-## Note: Superscrips are not read, has to be edited manually
-## writeLines(classifications[1], "test.txt")
-## classifications[1]
 ## ICD9-CM (ICD10-CM)
+## TODO: Extract chapter names [A-Z].* ([0-9]{1,})\n
 
+## writeLines(
+##     grep("(?<=\n)[A-Z].*\\([0-9]{1,}(?=\\)\n)", text, value = T, perl = T)
+##   , "chapters.txt")
+## text <- "\nV62.9 (Z65.9) Unspecified Problem Related to Unspecified Psychosocial\nCircumstances (725)\nV15.49 Z91.49 TEST\nV15.59 (Z91.5)\nV62.22 (Z91.82)\nV15.89 (Z91.89)\nV69.9 (Z72.9)\nV71.01 (Z72.811)\nV71.02 (Z72.810)\nOther Circumstances of Personal History (726)\nOther Personal History of Psychological Trauma (726)\nPersonal History of Self-Harm (726)\nPersonal History of Military Deployment (726)\nOther Personal Risk Factors (726)\nProblem Related to Lifestyle (726)\nAdult Antisocial Behavior (726)\nChild or Adolescent Antisocial Behavior (726)\nProblems Related to Access to Medical and Other Health Care (726)\nV63.9 (Z75.3) Unavailability or Inaccessibility of Health Care Facilities (726) \nV63.8 (Z75.4) Unavailability or Inaccessibility of Other Helping Agencies (726)\nNonadherence to Medical Treatment (726)\n"
+## gsub("((?<=\n).*\\)(?=\n))", "## \\1", classifications, perl=T)
 
 ## Preface ==============================
 preface <- extract_text("/home/eee/Dropbox/psykiatri/documents/dsm-5-manual-2013.pdf", pages = 41:44) ## classifications list start on p13
