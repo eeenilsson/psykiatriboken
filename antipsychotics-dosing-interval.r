3de3ff68eff75448497a6c93ad7710b96ebb58cd
Erik Nilsson <eeenilsson@hotmail.com>
2019-04-28 17:05:36 +0200

    test plot

--- antipsychotics-dosing-interval.r
+++ antipsychotics-dosing-interval.r
@@ -2,10 +2,10 @@
 
 antipsychotics <- read_csv('../psykiatri_st/antipsychotics-annotated.csv')
 
-antipsychotics%>%   
+antipsychotics%>%
     filter(select == 1) -> antipsychotics
 
-antipsychotics%>%    
+antipsychotics%>%
     select(drug, dose_effective_min, dose_effective_target, dose_effective_limit, dose_max)%>%
     filter(!drug %in% c("Perphenazine", "Zuclopenthixol"))-> tmp
 
@@ -48,15 +48,72 @@ tmp%>%
 
 p <- ggplot(aes(x = drug, y = 100), data = tmp)
 
-p + geom_point() + labs(y = "Dose (% of target dose)", x = "Antipsychotic") +
+p + geom_point() +
+    labs(y = "Dose (% of target dose)", x = "Antipsychotic") +
     geom_errorbar(aes(ymin = dose_effective_min, ymax = dose_effective_limit)) +
-    geom_point(aes(y = dose_max), color = "red") +
-    geom_text(aes(y = 750, label = tmp$dosebracket)) + 
+    geom_point(aes(y = dose_max), color = "red", show.legend = TRUE) +
+    geom_text(aes(y = 750, label = tmp$dosebracket)) +
     coord_flip() +
     theme_classic() -> p
+
+p + scale_fill_identity(name = 'the fill', guide = 'legend',labels = c('m1')) +
+    theme(legend.position="bottom")
 
 ## pdf(file = "fig_antipsychotics_dose_interval.pdf")
 ## p
 ## dev.off()
 
 
+p <- ggplot(data = tmp)
+
+p +
+    geom_point(aes(x = drug, y = dose_max, color = "red", shape = "red")) +
+    geom_point(aes(x = drug, y = 100, color = "cornflowerblue", shape = "cornflowerblue")) +
+    labs(y = "Dose (% of target dose)", x = "Antipsychotic") +
+    geom_errorbar(aes(x = drug, ymin = dose_effective_min, ymax = dose_effective_limit, color = "black")) +
+    geom_text(aes(x = drug, y = 750, label = tmp$dosebracket)) +
+    coord_flip() +
+    theme_classic() +
+    theme(legend.position="bottom") -> p
+p
+
+p + scale_colour_manual(name = 'test',
+                        values =c('black'='black','red'='red', 'cornflowerblue' = 'cornflowerblue'),
+                        labels = c('Effective dose range','Target dose', "Maximum dose")) -> pf
+
+pf +
+    scale_shape_manual(name = 'test',
+                       c('black'=1, 'red'=2, 'cornflowerblue' = 3),
+                       labels = c('Effective dose range','Target dose', "Maximum dose"))
+
+?scale_colour_manual
+
+
+
+## p + scale_fill_identity(guide = 'legend', labels = c(target = 'm1')) +
+##     theme(legend.position="bottom") +
+##     theme(legend.title=element_blank())
+
+## test longdata
+## names(tmp)
+## tmp%>%
+##     gather(key, value, dose_effective_limit:position_text) -> tmpl
+## tmp_range = tmp%>%select(drug, dosebracket, dose_effective_min, dose_effective_limit)
+
+## p <- ggplot(aes(x = drug), data = tmpl)
+
+
+
+## p +
+##     geom_point(aes(x = drug, y = value, fill = key), data = subset(tmpl, key == "dose_max")) +
+##     geom_point(aes(x = drug, y = 100, fill = key), data = subset(tmpl, key == "dose_max")) +
+##     geom_errorbar(aes(x = drug, ymin = dose_effective_min, ymax = dose_effective_limit, data = tmp_range, color = "black") +
+##     geom_text(aes(x = drug, y = 750, label =  tmp_range$dosebracket), data =  tmp_range) +
+##     coord_flip() +
+##     theme_classic() +
+##     labs(y = "Dose (% of target dose)", x = "Antipsychotic") +
+##     theme(legend.position="bottom")
+
+
+## -> p
+## p
