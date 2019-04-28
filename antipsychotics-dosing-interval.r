842e3d20d13bd31dfe39f736efee87e00e0ec3a7
enilsson <eeenilsson@hotmail.com>
2019-04-28 19:44:36 +0200

    working

--- antipsychotics-dosing-interval.r
+++ antipsychotics-dosing-interval.r
@@ -63,6 +63,7 @@ p + scale_fill_identity(name = 'the fill', guide = 'legend',labels = c('m1')) +
 ## p
 ## dev.off()
 
+require(tidyverse)
 
 p <- ggplot(data = tmp)
 
@@ -70,25 +71,24 @@ p +
     geom_point(aes(x = drug, y = dose_max, color = "red", shape = "red")) +
     geom_point(aes(x = drug, y = 100, color = "cornflowerblue", shape = "cornflowerblue")) +
     labs(y = "Dose (% of target dose)", x = "Antipsychotic") +
-    geom_errorbar(aes(x = drug, ymin = dose_effective_min, ymax = dose_effective_limit, color = "black")) +
     geom_text(aes(x = drug, y = 750, label = tmp$dosebracket)) +
     coord_flip() +
     theme_classic() +
     theme(legend.position="bottom") -> p
+
+p + scale_colour_manual(name = '',
+                        values =c('red'='red', 'cornflowerblue' = 'cornflowerblue'),
+                        labels = c('Target dose', 'Maximum dose')) +
+    scale_shape_manual(name = '',
+                       values = c('red' = 2, 'cornflowerblue' = 3),
+                       labels = c('Target dose', 'Maximum dose')) -> p
 p
+p + geom_errorbar(aes(x = drug, ymin = dose_effective_min, ymax = dose_effective_limit))
 
-p + scale_colour_manual(name = 'test',
-                        values =c('black'='black','red'='red', 'cornflowerblue' = 'cornflowerblue'),
-                        labels = c('Effective dose range','Target dose', "Maximum dose")) -> pf
 
-pf +
-    scale_shape_manual(name = 'test',
-                       c('black'=1, 'red'=2, 'cornflowerblue' = 3),
-                       labels = c('Effective dose range','Target dose', "Maximum dose"))
-
+?geom_line
 ?scale_colour_manual
-
-
+?scale_shape_manual
 
 ## p + scale_fill_identity(guide = 'legend', labels = c(target = 'm1')) +
 ##     theme(legend.position="bottom") +
@@ -100,9 +100,9 @@ pf +
 ##     gather(key, value, dose_effective_limit:position_text) -> tmpl
 ## tmp_range = tmp%>%select(drug, dosebracket, dose_effective_min, dose_effective_limit)
 
-## p <- ggplot(aes(x = drug), data = tmpl)
+## unique(tmpl$key)
 
-
+## p <- ggplot(aes(x = drug, y = value, data = tmpl)
 
 ## p +
 ##     geom_point(aes(x = drug, y = value, fill = key), data = subset(tmpl, key == "dose_max")) +
@@ -117,3 +117,17 @@ pf +
 
 ## -> p
 ## p
+
+
+    errors=matrix(c(-3.800904,-3.803444,-3.805985,-3.731204,-3.743969,
+                -3.756735,-3.742510,-3.764961,-3.787413,-3.731204,-3.743969,-3.756735,
+                -3.711420,-3.721589,-3.731758,-3.731204,-3.743969,-3.756735,-3.636346,
+                -3.675159,-3.713971,-3.731204,-3.743969,-3.756735),nrow=4,byrow=TRUE)
+
+errors = rbind(errors[, 1:3], errors[,4:6]) # manually reshaping the data
+modelName=c("model 1","model 2","model 3","model 0")
+type = rep(c("model", "obs"), each = 4)
+
+boxdata=data.frame(errors,modelName, type)
+
+colnames(boxdata)=c("icp","pred","icm","model", "type")
